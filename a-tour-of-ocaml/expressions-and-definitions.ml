769c86adbf7a110c56c60b3c1e387a1b169527aa
Seong Yong-ju <yongju.seong@codetakt.com>
2023-11-25 08:55:20 +0900

    add a-tour-of-ocaml/expressions-and-definitions.ml

---
+++ a-tour-of-ocaml/expressions-and-definitions.ml
@@ -0,0 +1,27 @@
+50 * 50;;
+
+6.28;;
+
+"This is really disco!";;
+
+true;;
+
+let u = [1; 2; 3; 4];;
+
+["this"; "is"; "mambo"];;
+
+9 :: u;;
+
+2 * if "hello" = "world" then 3 else 5;;
+
+let x = 50;;
+
+x * x;;
+
+let y = 50 in y * y;;
+
+let a = 1 in
+let b = 2 in
+  a + b;;
+
+let dummy = "hi" = "hello";;
